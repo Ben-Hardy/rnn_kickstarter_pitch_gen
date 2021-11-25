eb312a4dfc6fbf4b601ff78659890552a982b964
Ben Hardy <hardybe92@gmail.com>
2021-11-25 16:38:14 -0600

    cleaned up data fetching file a bit. moving generator file from a notebook to a python script so it can be run easily

--- data_fetch.py
+++ data_fetch.py
@@ -11,7 +11,6 @@ import tensorflow as tf
 site_url = 'https://webrobots.io/kickstarter-datasets/'
 data_path = './data/'
 
-
 request = requests.get(site_url)
 
 soup = BeautifulSoup(request.content, features='html.parser')
@@ -27,7 +26,10 @@ for i in urls_to_download:
 		final_urls.append(i)
 
 '''
-Please note: This is currently only set up to grab the last entry of data for 2020. You could technically set it up to grab every month's data for each year, but it appears successive sets of data are cumulative so there's not really a point to grabbing sets from earlier in the year. 2020 alone provides over 200000 kickstarter names and blurbs, more than enough for our RNN text generator to learn from
+Please note: This is currently only set up to grab the last entry of data for 2020. You could technically set it up to 
+grab every month's data for each year, but it appears successive sets of data are cumulative so there's not really a 
+point to grabbing sets from earlier in the year. 2020 alone provides over 200000 kickstarter names and blurbs, more 
+than enough for our RNN text generator to learn from.
 '''
 
 final_urls = [i for i in final_urls if "2020" in i]
@@ -43,14 +45,10 @@ files_to_process = os.listdir(data_path)
 output_df = pd.DataFrame()
 cols_to_take = ['blurb', 'name']
 
-
-
-
 for f in files_to_process:
 	df = pd.read_csv(data_path + f)
 	output_df = output_df.append(df[cols_to_take])
 	os.remove(data_path + f)
-
 
 print(output_df.shape)
 output_df.to_csv('kickstarter_data.csv', encoding='utf-8', index=False)
