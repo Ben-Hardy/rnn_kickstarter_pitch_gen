907699e9c6f8809e4c03b302238014580721ad71
Ben Hardy <hardybe92@gmail.com>
2021-11-25 11:18:06 -0600

    finished off the data grabbing script. it now successfully grabs a dataset from webrobots.io, unzips it, then takes the relevant columns from the CSVs and chucks them into a final output csv that we'll be using to feed the RNN

--- data_fetch.py
+++ data_fetch.py
@@ -1,14 +1,15 @@
 import requests
 import csv
-import re
 from bs4 import BeautifulSoup
 import sys
+import os
+import pandas as pd
 
 from zipfile import ZipFile
 
 import tensorflow as tf
 site_url = 'https://webrobots.io/kickstarter-datasets/'
-data_path = './data'
+data_path = './data/'
 
 
 request = requests.get(site_url)
@@ -25,7 +26,9 @@ for i in urls_to_download:
 	if ".zip" in i:
 		final_urls.append(i)
 
-
+'''
+Please note: This is currently only set up to grab the last entry of data for 2020. You could technically set it up to grab every month's data for each year, but it appears successive sets of data are cumulative so there's not really a point to grabbing sets from earlier in the year. 2020 alone provides over 200000 kickstarter names and blurbs, more than enough for our RNN text generator to learn from
+'''
 
 final_urls = [i for i in final_urls if "2020" in i]
 
@@ -35,3 +38,19 @@ print(path_to_file)
 zf = ZipFile(path_to_file)
 zf.extractall(path='./data')
 
+
+files_to_process = os.listdir(data_path)
+output_df = pd.DataFrame()
+cols_to_take = ['blurb', 'name']
+
+
+
+
+for f in files_to_process:
+	df = pd.read_csv(data_path + f)
+	output_df = output_df.append(df[cols_to_take])
+	os.remove(data_path + f)
+
+
+print(output_df.shape)
+output_df.to_csv('kickstarter_data.csv', encoding='utf-8', index=False)
